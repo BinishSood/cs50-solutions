f57aef20c4b637a8f2b4d731cb98bb9a20f3ccc1
BinishSood <quantumxaura@gmail.com>
2025-08-24 22:47:30 +0530

    week 5 messed me up

--- week1/labs -week1/debug 6.41.19 PM.c
+++ week1/labs -week1/debug 6.41.19 PM.c
@@ -1,13 +1,16 @@
 // Become familiar wih C syntax
 // Learn to debug buggy code
 #include <stdio.h>
-#include <cs50.h>
 
 int main(void)
 {
     // Ask for your name and where live
-    string name = get_string("What is your name? ");
-    string location = get_string("Where do you live? ");
+    char *name;
+    char *location;
+    printf("What is your name? ");
+    scanf("%s", name);
+    printf("Where do you live? ");
+    scanf("%s", location);
 
     // Say hello
     printf("Hello, %s, from %s!\n", name, location);
---
+++ week5/problemset-week5/speller/dictionary.c
@@ -0,0 +1,108 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
+// TODO: Choose number of buckets in hash table
+const unsigned int N = 5000;
+
+unsigned int word_count = 0;
+
+// Hash table
+node *table[N];
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // TODO
+    unsigned int index = hash(word);
+    node *cursor = table[index];
+
+    while (cursor != NULL)
+    {
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    // TODO: Improve this hash function
+    unsigned int sum = 0;
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        sum += tolower(word[i]);
+    }
+    return sum % N;
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    // TODO
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+    char buffer[LENGTH + 1];
+    while (fscanf(file, "%s", buffer) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return false;
+        }
+
+        strcpy(n->word, buffer);
+        unsigned int index = hash(buffer);
+
+        n->next = table[index];
+        table[index] = n;
+        word_count++;
+    }
+    fclose(file);
+    return true;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return word_count;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    // TODO
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+        while (cursor != NULL)
+        {
+            node *tmp = cursor;
+            cursor = cursor->next;
+            free(tmp);
+        }
+    }
+    return true;
+}
